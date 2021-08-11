a5f0c831869e145d8381dcf602aabd3db6f64dab
Luiz C. M. de Aquino <aquino.luizclaudio@gmail.com>
2021-08-10 21:08:02 -0300

    Adiciona o total de municipios na tabela de minimo e maximo.

--- gerarTabelas.py
+++ gerarTabelas.py
@@ -42,22 +42,23 @@ estado = [
     "PA", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO",
     "ZZ"
 ]
-
 for a in ano:
     for t in turno:
+        totalDeMunicipios = 0
         nomeDoVotavel = temp[a][t]
         df = {}
         for nome in nomeDoVotavel:
-            df[nome] = {"MIN": 1.0e+10, "MAX": -1.0e+10, "QTD" : 0}
+            df[nome] = {"MIN": 1.0e+10, "MAX": -1.0e+10, "QTD_MUNICIPIOS" : 0}
 
         print(f"TABELA MÍNIMO E MÁXIMO DE VOTOS NO TURNO {t} ANO {a}\n")
         for es in estado:
             ap = f"{dirbase}{a}/apuracao-t{t}-{a}-{es}.csv"
             dfAux = pd.read_csv(ap, encoding = codificacao, delimiter = delimitador)
+            totalDeMunicipios += len(dfAux["NM_MUNICIPIO"].unique())
             for nome in nomeDoVotavel:
                 votos = dfAux.loc[(dfAux["NM_VOTAVEL"] == nome) & (dfAux["QT_VOTOS"] > 0)]
                 if(len(votos) > 0):
-                    df[nome]["QTD"] += len(votos)
+                    df[nome]["QTD_MUNICIPIOS"] += len(votos)
                     if(nome in df.keys()):
                         if(votos['QT_VOTOS'].min() < df[nome]["MIN"]):
                             df[nome]["MIN"] = votos['QT_VOTOS'].min()
@@ -66,6 +67,6 @@ for a in ano:
                             df[nome]["MAX"] = votos['QT_VOTOS'].max()
 
         for nome in df.keys():
-            print(f"{nome} {df[nome]['MIN']} {df[nome]['MAX']} {df[nome]['QTD']}")
+            print(f"{nome} {df[nome]['MIN']} {df[nome]['MAX']} {(100.0*df[nome]['QTD_MUNICIPIOS']/totalDeMunicipios):4.2f}")
 
         print("")
